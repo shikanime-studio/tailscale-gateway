606fa106c82f16158754e3382c31fb5ed3900b9c
William Phetsinorath <william.phetsinorath@shikanime.studio>
2026-01-04 02:54:19 +0100

    Update
    [ghstack-poisoned]

--- internal/reconcilerutil/crtl.go
+++ internal/reconcilerutil/crtl.go
@@ -7,9 +7,6 @@ import ctrl "sigs.k8s.io/controller-runtime"
 func JoinResults(results ...ctrl.Result) ctrl.Result {
 	var res ctrl.Result
 	for _, r := range results {
-		if r.Requeue {
-			res.Requeue = true
-		}
 		if r.RequeueAfter > 0 {
 			res.RequeueAfter = r.RequeueAfter
 		}
